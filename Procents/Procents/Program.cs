80344394e4cf9f46c3cafc2d72946905b182e5bb
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2019-09-12 18:08:05 +0500

    Add files via upload

---
+++ Procents/Procents/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите исходную сумму");
+            double originalAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите процент");
+            int interestRate = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите срок вклада в месяц");
+            int depositTerm = int.Parse(Console.ReadLine());
+            double total = Calculate(originalAmount, interestRate, depositTerm);
+            Console.WriteLine(total);
+        }
+        public static double Calculate(double originalAmount, int interestRate, int depositTerm)
+        {
+            return originalAmount + (interestRate / 12.0) * depositTerm;
+        }
+    }
+}
