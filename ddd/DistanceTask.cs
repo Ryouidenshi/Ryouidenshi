9fe0d24518b155acdd736b6add122ab0525bb628
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2019-09-20 00:49:31 +0500

    Add files via upload

---
+++ ddd/DistanceTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DistanceTask
+{
+    public static class DistanceTask
+    {
+        // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+        public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+        {
+            double h = Math.Abs((((bx - ax) * (y - ay)) - ((by - ay) * (x - ax))) / (Math.Sqrt(((bx - ax) * (bx - ax)) + ((by - ay) * (by - ay)))));
+            double toFirstDot = Math.Abs(Math.Sqrt(Math.Pow(ax - x, 2) + Math.Pow(ay - y, 2)));
+            double toSecondDot = Math.Abs( Math.Sqrt(Math.Pow(bx - x, 2) + Math.Pow(by - y, 2)));
+            return Math.Min(toFirstDot, Math.Min(toSecondDot, h));           
+        }
+    }
+}
